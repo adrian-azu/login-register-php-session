c09fe99307f2639eb2a8b35b690ca4d25537a6a6
Azu <azucenaadrian00@gmail.com>
2020-04-11 05:52:05 +0800

    New Stuffs

--- controller.php
+++ controller.php
@@ -1,21 +1,23 @@
 <?php
-session_start();
-include( "config.php");
-global $servername;
-global $username;
-global $password;
-global $dbname;
-global $conn;
-global $user;
-global $errors=array();;
-global $newuser,$fname,$lname,$pass1,$pass2,$msg;
+session_start(); //start session for the user
+include( "config.php"); //include database connection
+$update=false;
+$errors=array();
+$user="";
+$pass="";
+$newuser="";
+$fname= $lname= $pass1= $pass2="";
+$msg="";
+$roles="";
+$id=0;
 
-global $msg="";
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-  if (isset($_POST['Login'])) {
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){ //check if request is POST method
+  if (isset($_POST['Login'])) { //Check if POST login is triggered
     login();
   }
 }
+
 function login(){
   global $servername;
   global $username;
@@ -35,36 +37,28 @@ $id= $username= $password= $firstname=$roles= $lastname="";
       $pass=trim($_POST['password']);
   }
   if(strlen($user)>0 && strlen($pass)>0 && empty($errors)){
-    $sql = "SELECT * FROM users WHERE username=?";
-    $stmt = $conn->prepare($sql);
+    $stmt = $conn->prepare("SELECT * FROM users WHERE username=?");
     $stmt->bind_param("s", $param_username);
     $param_username = $user;
-    if($stmt->execute()){
+    if($stmt->execute()){ //if successfully executed
     $stmt->store_result();
     }
     if($stmt->num_rows == 1){
       $stmt->bind_result($id,$username,$password,$roles,$firstname,$lastname);
       $stmt->fetch();
-      echo $username;
-      echo "<br>";
-      echo $stmt->bind_result($id,$username,$password,$roles,$firstname,$lastname);
-      echo $password;
-      echo "<br>";
-      echo $pass;
-      echo "<br>";
-      echo md5($pass);
       $pass=md5($pass);
       if($pass===$password)
        {
+         session_start(); //start session for the user
          if($roles=='admin' || $roles=='Admin'){
-           $_SESSION['user']=$user;
+           $_SESSION['user']=$user; //store username and id to session
            $_SESSION['userid']=$id;
              header("location: admin/admin.php");
          }else{
+           session_start(); //start session for the user
            $_SESSION['user']=$user;
            $_SESSION['userid']=$id;
            $_SESSION['Username']=$username;
-           echo $_SESSION['user'];
            header("location: welcome.php");
          }
          }else{
@@ -92,7 +86,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 }
 
 function register(){
-
+  global $servername;
+  global $username;
+  global $password;
+  global $dbname;
+  global $conn;
+  global $user, $errors;
+  global $newuser,$fname,$lname,$pass1,$pass2,$msg;
+  $roles="User";
 if (empty(trim($_POST['firstname']))){
   array_push($errors,"First Name is required");
 }else{
@@ -129,14 +130,15 @@ if(empty($errors)){
     array_push($errors, "Username already taken");
   }else{
     $pass1=md5($pass1);
-    $sql = "INSERT INTO users (username, password, firstname, lastname)
-    VALUES ('$newuser', '$pass1', '$fname', '$lname')";
+    $sql = "INSERT INTO users (roles,username, password, firstname, lastname)
+    VALUES ('$roles','$newuser', '$pass1', '$fname', '$lname')";
     if ($conn->query($sql) === TRUE) {
       $msg="successfully created!";
       $_SESSION['firstname']=$fname;
       $_SESSION['lastname']=$lname;
       $_SESSION['username']=$newuser;
       $_SESSION['passw']=$pass1;
+
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
       }
@@ -146,27 +148,191 @@ if(empty($errors)){
 }
 function islogin(){
   if (empty($_SESSION['user'])){
-    session_destroy();
-    unset($_SESSION['user']);
+    session_destroy(); //Remove session
+    unset($_SESSION['user']); //Delete user session
     header("location: ../index.php");
   }
 }
-
 if (isset($_GET['logout'])) {
 	session_destroy();
 	unset($_SESSION['user']);
 	header("location: ../index.php");
   echo $_SESSION['user'];
 }
-function fillin(){
+
+if(isset($_GET['delete'])){
+$id=$_GET['delete'];
+  $conn->query("DELETE FROM users WHERE id=$id")or die($conn->error());
+  $_SESSION['message']="User Deleted";
+}
+
+if(isset($_GET['edit']) && !empty($_GET['edit'])){
+
+  $sql="SELECT * FROM users WHERE id=?";
+  if ($stmt=$conn->prepare($sql)) {
+    $stmt->bind_param("i",$param_id);
+    $param_id=$_GET["edit"];
+      if($stmt->execute()){
+        $result=$stmt->get_result();
+        if ($result->num_rows==1) {
+          $row = $result->fetch_array(MYSQLI_ASSOC);
+          $fname=$row['firstname'];
+          $lname=$row['lastname'];
+          $user=$row['username'];
+          $roles=$row['roles'];
+          $update=true;
+        }else{
+          array_push($errors, $stmt->error);
+        }
+      } else {
+        array_push($errors, "Something went wrong" . $stmt->error);
+      }
+    }
+  }
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+  if(isset($_POST["save"])){
+    if (isset($_POST["firstname"])) {
+      $fname=$_POST['firstname'];
+    }else{
+      array_push($errors, "First Name is required!");
+    }
+
+    if (isset($_POST["lastname"])) {
+      $lname=$_POST['lastname'];
+    }else{
+      array_push($errors, "Last Name is required!");
+    }
+
+    if (isset($_POST["roles"])) {
+      $roles=$_POST['roles'];
+    }else{
+      array_push($errors,"Kindly select a role");
+    }
+
+    if (isset($_POST["user"])) {
+      $newuser=$_POST['user'];
+    }else{
+      array_push($errors,"Kindly select a role");
+    }
+
+    if (isset($_POST["password"])) {
+      $pass=$_POST['password'];
+    }else{
+      array_push($errors,"No Password found");
+    }
+
+    if (empty($errors)) {
+      $sql_u = "SELECT * FROM users WHERE username=?";
+      $stmt=$conn->prepare($sql_u);
+      $stmt->bind_param("s", $param_username);
+      $param_username = $newuser;
+
+      if ($stmt->execute()) {
+        $stmt->store_result();
+
+      }if($stmt->num_rows >0){
+        array_push($errors, "Username is already taken");
+
+      }else{
+        $sql="INSERT INTO users (roles,username, password, firstname, lastname)
+        VALUES (?,?,?,?,?)";
+        $stmt=$conn->prepare($sql);
+        $stmt->bind_param("sssss", $param_roles,$param_user,$param_pass,$param_fname,$param_lname);
+        $param_roles = $roles;
+        $param_user = $newuser;
+        $param_pass = md5($pass);
+        $param_fname = $fname;
+        $param_lname = $lname;
+        if($stmt->execute()){
+          $_SESSION['message']="New account saved!";
+        }else{
+          array_push($errors,$stmt->error);
+        }
+      }
+    }
+  }
+
+  if(isset($_POST['update'])){
+      update();
+    }
+}
+function update(){
   global $servername;
   global $username;
   global $password;
   global $dbname;
   global $conn;
-  global $errors,$user,$pass;
+  global $user, $errors;
   global $newuser,$fname,$lname,$pass1,$pass2,$msg;
+  if(isset($_POST['firstname']) && !empty($_POST['firstname'])) {
+    $fname=$_POST['firstname'];
 
+  }else{
+    array_push($errors, "First name is required");
 
+  }
+
+  if(isset($_POST['lastname']) && !empty($_POST['lastname'])){
+    $lname=$_POST['lastname'];
+
+  }else {
+    array_push($errors, "Last Name is required");
+
+  }
+  if (isset($_POST['roles']) && !empty($_POST['roles'])) {
+    $roles=$_POST['roles'];
+
+  }else{
+  array_push($errors, "No Roles selected");
+
+  }
+
+  if (isset($_POST['user']) && !empty($_POST['user'])) {
+      $user=$_POST['user'];
+
+  }else{
+    array_push($errors, "Username is required");
+
+  }
+  if (isset($_POST['password']) && !empty($_POST['password'])) {
+    $pass=$_POST['password'];
+
+  }else{
+    array_push($errors, "No password is found");
+
+  }
+
+  if(empty($errors)){
+
+    $pass=md5($pass);
+    echo $fname;
+    $sql_u="SELECT * FROM users WHERE id=?";
+    $stmt = $conn->prepare($sql_u);
+    $stmt->bind_param("i", $param_id);
+    $param_id=$id;
+    if($stmt->execute()){
+      $stmt->store_result();
+    }if($stmt->num_rows == 1){
+      $stmt->bind_result($id,$username,$password,$roles,$firstname,$lastname);
+      $stmt->fetch();
+      $pass=md5($pass);
+      if($pass===$password)
+      {
+        if($pass1==$pass2){
+          $pass=md5($pass1);
+          $sql = "UPDATE users SET firstname=$fname, lastname=$lname, roles=$roles,password=$pass WHERE username=?";
+          $stmt = $conn->prepare($sql);
+          $conn->query($sql);
+          $_SESSION['message'] = "Account updated!";
+        }
+        else{
+          array_push($errors, "New Password not match");
+        }
+      }else{
+        array_push($errors, "Incorrect Password");
+      }
+    }
+  }
 }
+
 ?>
--- welcome.php
+++ welcome.php
@@ -1,5 +1,5 @@
 <?php include('controller.php');
-echo "Hello" ." ". $newuser . ".";
+
 if (isset($_GET['log-out'])) {
   session_destroy();
   unset($_SESSION['user']);
@@ -14,22 +14,98 @@ if (empty($_SESSION['user'])){
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="welcomestyle.css">
     <title></title>
   </head>
   <body>
+    <?php if (isset($_SESSION['message'])): ?>
+	<div class="msg">
+		<?php
+			echo $_SESSION['message'];
+			unset($_SESSION['message']);
+		?>
+	</div>
+<?php endif ?>
     <div class="container">
-      <div class="header">
-        <h1>USER INTERFACE</h1>
-        <li><a href="welcome.php?log-out='1'">log-out</a></li>
-        
-        <input type="submit" name="edit" value="Change password">
-      </div>
       <div class="low-header">
-
+        <li><a href="welcome.php?log-out='1'">log-out</a></li>
+        </div>
+      <div class="header">
+        <?php require_once "config.php";
+        $result=$conn->query("SELECT id,firstname, lastname, roles, username FROM users");
+        ?>
+        <div class="table-row">
+          <table class="table">
+            <thead>
+                <tr>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Roles</th>
+                  <th>Username</th>
+                  <th colspan="4">Action</th>
+              </tr>
+            </thead>
+            <?php
+            while($row = $result->fetch_assoc()):
+             ?>
+             <tr>
+               <td><?php echo $row['firstname']; ?></td>
+               <td><?php echo $row['lastname']; ?></td>
+               <td><?php echo $row['roles']; ?></td>
+               <td><?php echo $row['username']; ?></td>
+               <td><a href="welcome.php?edit= <?php echo $row['id']; ?>"class="edit_btn">EDIT</a></td>
+               <td><a href="welcome.php?delete=<?php echo $row['id']; ?>" class="del_btn">DELETE</a></td>
+             </tr>
+           <?php endwhile; $result->free();  ?>
+          </table>
+        </div>
       </div>
+          <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            <div class="form-group">
+              <label>First Name</label>
+              <input type="text" name="firstname" value="<?php echo $fname ?>">
+            </div>
+            <div class="form-group">
+              <label>Last Name</label>
+              <input type="text" name="lastname" value="<?php echo $lname ?>">
+            </div>
+            <div class="form-group">
+              <label>Role</label>
+              <?php if($update==false): ?>
+              <select name="roles">
+                    <option hidden disabled selected value> -- select an option -- </option>
+                    <option value="Admin">Admin</option>
+                    <option value="User">User</option>
+                      </select>
+                  <?php else: ?>
+                    <select name="roles">
+                      <?php if($roles==="user" || "User"): ?>
+                        <option value="<?php echo $roles; ?>"><?php echo $roles; ?></option>
+                        <option value="Admin">Admin</option>
+                      <?php else: ?>
+                        <option value="<?php echo $roles; ?>"><?php echo $roles; ?></option>
+                        <option value="User">User</option>
+                      <?php endif ?>
+              </select>
+              <?php endif ?>
+            </div>
+            <div class="form-group">
+              <input type="hidden" name="id" value="<?php echo $id ?>">
+              <label>Username</label>
+              <input type="text" name="user" value="<?php echo $user ?>">
+            </div>
+            <div class="form-group">
+              <label>Password</label>
+              <input type="password" name="password" placeholder="Enter your old password">
+            </div>
+            <div class="form-group">
+              <?php if($update==true): ?>
+                <button class="btn" type="submit" name="update">UPDATE</button>
+              <?php else: ?>
+                <button class="btn" type="submit" name="save">SAVE</button>
+              <?php endif ?>
+          </div><?php display_error(); ?>
+      </form>
     </div>
-
-
-
   </body>
 </html>
